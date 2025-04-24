94d5e910fa32cbd2de05583c691310b9a0ecd2db
Davi Brito <davibrito1282@gmail.com>
2025-04-24 17:13:40 -0300

    Refactor hierarchy to introduce Empregado superclass.
    Extracted common attributes and methods from Funcionario and Gerente into a new Empregado superclass to improve code reuse. Updated Relatorio to handle the new hierarchy, simplifying the method for generating employee reports.

--- learning-java-api/src/Gerente.java
+++ learning-java-api/src/Gerente.java
@@ -1,29 +1,3 @@
-public class Gerente {
-    private String nome;
-    private int idade;
-    private double bonus;
+public class Gerente extends Empregado {
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public double getBonus() {
-        return bonus;
-    }
-
-    public void setBonus(double bonus) {
-        this.bonus = bonus;
-    }
 }
--- learning-java-api/src/Relatorio.java
+++ learning-java-api/src/Relatorio.java
@@ -1,23 +1,13 @@
 public class Relatorio {
-        public void relatorioFuncionario(Funcionario funcionario){
+        public void relatorioFuncionario(Empregado empregado){
         System.out.println("========================================\n");
         System.out.println("        RELATÓRIO DO FUNCIONÁRIO        \n");
         System.out.println("========================================\n");
-        System.out.println(String.format("Nome: %s\n", funcionario.getNome()));
-        System.out.println(String.format("Idade: %d\n", funcionario.getIdade()));
-        System.out.println(String.format("Salário:  R$ %.2f\n", funcionario.getSalario()));
+        System.out.println(String.format("Nome: %s\n", empregado.getNome()));
+        System.out.println(String.format("Idade: %d\n", empregado.getIdade()));
+        System.out.println(String.format("Salário:  R$ %.2f\n", empregado.getSalario()));
         System.out.println("========================================\n");
                         
     }
-    
-        public void relatorioGerente(Gerente gerente){
-        System.out.println("========================================\n");
-        System.out.println("        RELATÓRIO DO GERENTE        \n");
-        System.out.println("========================================\n");
-        System.out.println(String.format("Nome: %s\n", gerente.getNome()));
-        System.out.println(String.format("Idade: %d\n", gerente.getIdade()));
-        System.out.println(String.format("Salário:  R$ %.2f\n", gerente.getBonus()));
-        System.out.println("========================================\n");
 
-    }
 }
